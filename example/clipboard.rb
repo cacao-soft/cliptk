eb9bddf1d1e19f947c4649fab91392ba31c7da53
CACAO <76836489+cacao-soft@users.noreply.github.com>
2023-03-12 00:37:37 +0900

    サンプルを追加

---
+++ example/clipboard.rb
@@ -0,0 +1,35 @@
+#
+# クリップボードへの入出力
+#
+
+format = Clipboard.format
+if Clipboard.type == :MZ
+  # MV 用に変換するにはフォーマットの指定が必要
+  Clipboard(:MV, format, "UNICODETEXT") << Clipboard.data
+else
+  Clipboard << Clipboard.data
+end
+
+=begin
+
+  Clipboard << データ
+  Clipboard(*フォーマット) << データ
+  Clipboard.set(*フォーマット, データ)
+    クリップボードにデータを書き込み
+    フォーマットを省略した場合は、VXAce か MZ のデータとして書き込みます。
+      :XP, :VX, :VXA, :MV, :MZ, "UNICODETEXT"
+    XP～VXAは、すべての形式で保持するので指定する意味は薄いかと思います。
+    自動判別は VXAce と MZ のデータ構造を使用します。
+
+  Clipboard.data
+    クリップボードのデータを取得します。
+    VXAce 以前は Rubyオブジェクト、MV 以降は JSON文字列で取得します。
+
+  Clipboard.type
+    クリップボードのデータ形式を取得します。
+      :XP, :VX, :VXA, :MV, :MZ
+
+  Clipboard.format
+    標準および登録済みのクリップボード形式を取得します。この値は変動します。
+
+=end
