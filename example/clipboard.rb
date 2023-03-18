a051f5a14f1bace0cba2636f41db0fde97156ebb
CACAO <76836489+cacao-soft@users.noreply.github.com>
2023-03-18 13:11:30 +0900

    v0.5.2

--- example/clipboard.rb
+++ example/clipboard.rb
@@ -5,7 +5,7 @@
 format = Clipboard.format
 if Clipboard.type == :MZ
   # MV 用に変換するにはフォーマットの指定が必要
-  Clipboard(:MV, format, "UNICODETEXT") << Clipboard.data
+  Clipboard(:MV, format, :TXT) << Clipboard.data
 else
   Clipboard << Clipboard.data
 end
@@ -17,7 +17,7 @@ end
   Clipboard.set(*フォーマット, データ)
     クリップボードにデータを書き込み
     フォーマットを省略した場合は、VXAce か MZ のデータとして書き込みます。
-      :XP, :VX, :VXA, :MV, :MZ, "UNICODETEXT"
+      :XP, :VX, :VXA, :MV, :MZ, :TXT
     XP～VXAは、すべての形式で保持するので指定する意味は薄いかと思います。
     自動判別は VXAce と MZ のデータ構造を使用します。
 
